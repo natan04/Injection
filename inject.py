cbe6652c3473874843cb065af9dc76cf17c0358c
natan04 <natan04@gmail.com>
2017-04-10 10:23:34 +0000

    change from relative to full

--- inject.py
+++ inject.py
@@ -3,7 +3,7 @@ import time
 import os
 import sys
 from scapy.all import *
-os.system("insmod 8812au-3.ko")
+os.system("/home/pi/injection/insmod 8812au-3.ko")
 time.sleep(5)
 os.system("iw dev wlan1 set type monitor")
 os.system("iw dev wlan1 set channel 64 HT20")  
@@ -12,7 +12,7 @@ pkts=rdpcap("abcd.pcap")
 while True:
 	if int(time.time()) > sended:
 		sended = int(time.time())	
-		os.system("iw dev wlan0 set channel 64 HT20")
-		sendp(pkts[0], iface="wlan0",  verbose=0)
+		os.system("iw dev wlan1 set channel 64 HT20")
+		sendp(pkts[0], iface="wlan1",  verbose=0)
 	time.sleep(0.05)
 	
