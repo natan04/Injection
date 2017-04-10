7a33f985f6947dd1be73b574fff46e9382977417
natan04 <natan04@gmail.com>
2017-04-10 10:27:32 +0000

    fix all relative

--- inject.py
+++ inject.py
@@ -3,12 +3,12 @@ import time
 import os
 import sys
 from scapy.all import *
-os.system("/home/pi/injection/insmod 8812au-3.ko")
+os.system("insmod  /home/pi/injection/8812au-3.ko")
 time.sleep(5)
 os.system("iw dev wlan1 set type monitor")
 os.system("iw dev wlan1 set channel 64 HT20")  
 sended = time.time()
-pkts=rdpcap("abcd.pcap")
+pkts=rdpcap("/home/pi/injection/abcd.pcap")
 while True:
 	if int(time.time()) > sended:
 		sended = int(time.time())	
