9d8ca6444411d37ea5ed683ae78aab5452fa7fe4
natan04 <natan04@gmail.com>
2017-04-10 10:18:30 +0000

    add sleep

--- inject.py
+++ inject.py
@@ -4,6 +4,7 @@ import os
 import sys
 from scapy.all import *
 os.system("insmod 8812au-3.ko")
+time.sleep(5)
 os.system("iw dev wlan1 set type monitor")
 os.system("iw dev wlan1 set channel 64 HT20")  
 sended = time.time()
